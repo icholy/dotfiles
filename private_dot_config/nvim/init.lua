f32007e7228da387c95f86c59b8eaca4e75c017f
Ilia Choly <ilia.choly@gmail.com>
2022-06-24 14:37:38 -0400

    nvim: show highest severity diagnostics first

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -332,6 +332,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     virtual_text = false,
     signs = true,
     update_in_insert = false,
+    severity_sort = true,
   }
 )
 

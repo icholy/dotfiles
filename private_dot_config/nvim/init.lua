82d1271f66e0b25900d38f7fbd5de42c10890a60
Ilia Choly <ilia.choly@gmail.com>
2022-09-14 12:11:52 -0400

    add keymap for opening terminal

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -399,5 +399,6 @@ vim.keymap.set("", "K", vim.lsp.buf.hover)
 vim.keymap.set("", "KK", function() vim.diagnostic.open_float(nil, {focus=false}) end)
 vim.keymap.set("n", "<Leader>.", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<Leader>l", ":set list!<CR>")
+vim.keymap.set("n", "<Leader>t", ":belowright split | terminal<CR>")
 
 

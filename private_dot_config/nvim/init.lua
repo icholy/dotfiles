2d75450bca87ee82a9423580123f390c2c893293
Ilia Choly <ilia.choly@gmail.com>
2023-01-20 08:54:14 -0500

    use sync format

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -41,9 +41,9 @@ local group = vim.api.nvim_create_augroup("MyGroup", { clear = true })
 
 -- format go files on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function () vim.lsp.buf.format({ async = true }) end,
   group = group,
   pattern = "*.go",
+  callback = function () vim.lsp.buf.format() end,
 })
 
 -- automatically enter insert mode

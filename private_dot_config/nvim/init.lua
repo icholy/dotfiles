8f1a4f50993d41e140f054290e8a65726e211c3c
Ilia Choly <ilia.choly@gmail.com>
2023-02-13 08:50:19 -0500

    nvim: rename lua lsp

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -134,7 +134,7 @@ require("packer").startup(function(use)
       local runtime_path = vim.split(package.path, ";")
       table.insert(runtime_path, "lua/?.lua")
       table.insert(runtime_path, "lua/?/init.lua")
-      lspconfig.sumneko_lua.setup({
+      lspconfig.lua_ls.setup({
         settings = {
           Lua = {
             runtime = {

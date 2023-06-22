41d0a2fb8c10f841c9e43d591cab04f68a216124
Ilia Choly <ilia.choly@gmail.com>
2023-06-22 08:49:30 -0400

    nvim: enable incremental selection

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -279,6 +279,15 @@ require("packer").startup(function(use)
             "lua",
             "bash"
           },
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = '<CR>',
+              scope_incremental = '<CR>',
+              node_incremental = '<TAB>',
+              node_decremental = '<S-TAB>',
+            },
+          },
         })
     end
   })

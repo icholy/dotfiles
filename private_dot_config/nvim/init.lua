7f1a5be32264cea736aace9dcf011bc17ec1b0b4
Ilia Choly <ilia.choly@gmail.com>
2023-06-23 08:37:38 -0400

    nvim: use <TAB> to start incremental selection

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -282,8 +282,7 @@ require("packer").startup(function(use)
           incremental_selection = {
             enable = true,
             keymaps = {
-              init_selection = '<CR>',
-              scope_incremental = '<CR>',
+              init_selection = '<TAB>',
               node_incremental = '<TAB>',
               node_decremental = '<S-TAB>',
             },

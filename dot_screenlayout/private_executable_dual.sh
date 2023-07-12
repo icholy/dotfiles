cee5187eb65429d45dc76d12efde738e97f8acff
Ilia Choly <ilia.choly@gmail.com>
2023-07-12 15:15:49 -0400

    xrandr: add layout

---
+++ dot_screenlayout/private_executable_dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 3440x0 --rotate left --output DP-2 --off --output DP-3 --off --output DP-4 --mode 3440x1440 --pos 0x282 --rotate normal --output DP-5 --off

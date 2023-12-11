3f0779592d7e268feee147669ce95fe4a8345ca2
Ilia Choly <ilia.choly@gmail.com>
2023-12-11 13:10:39 -0500

    nvim: add work-around for bad highlighting

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -46,6 +46,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function () vim.lsp.buf.format() end,
 })
 
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = 'solarized*',
+  command = 'highlight! link NormalFloat Pmenu',
+  group = group,
+})
+
 -- automatically enter insert mode
 vim.api.nvim_create_autocmd({"BufWinEnter", "WinEnter", "TermOpen"}, {
 	command = "startinsert",

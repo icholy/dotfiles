4b56db55a79f17fdd8decd6875a7c5d65f17f39f
Ilia Choly <ilia.choly@gmail.com>
2024-02-20 00:20:43 -0500

    nvim: clean up gotolinks

--- private_dot_config/nvim/lua/gotolink.lua
+++ private_dot_config/nvim/lua/gotolink.lua
@@ -5,42 +5,41 @@ function goto_link()
       vim.api.nvim_err_writeln(err.message)
       return
     end
+    local cursor = get_cursor_pos()
     for _, link in pairs(result) do
-      if is_cursor_in_range(link.range) then
-        goto_link_target(link.target)
+      if in_range(cursor, link.range) then
+        jump_to_link_target(link.target)
         return
       end
     end
   end)
 end
 
-function is_cursor_in_range(range)
+function get_cursor_pos()
   local cursor = vim.api.nvim_win_get_cursor(0)
   cursor[1] = cursor[1] - 1 -- Adjust line number for 0-indexing
   cursor[2] = vim.lsp.util.character_offset(0, cursor[1], cursor[2])
-  -- check if cursor line is in range
-  if (cursor[1] > range.start.line and cursor[1] < range['end'].line) then
+  return { line = cursor[1], character = cursor[2] }
+end
+
+function in_range(pos, range)
+  if (pos.line > range.start.line and pos[1] < range['end'].line) then
     return true
-  elseif cursor[1] == range.start.line and cursor[1] == range['end'].line then
-    -- when cursor and range are in the same line, check the character index
-    return cursor[2] >= range.start.character and cursor[2] <= range['end'].character
-  elseif cursor[1] == range.start.line then
-    return cursor[2] >= range.start.character
-  elseif cursor[1] == range['end'].line then
-    return cursor[2] <= range['end'].character
+  elseif pos.line == range.start.line and pos[1] == range['end'].line then
+    return pos.character >= range.start.character and pos[2] <= range['end'].character
+  elseif pos.line == range.start.line then
+    return pos.character >= range.start.character
+  elseif pos.line == range['end'].line then
+    return pos.character <= range['end'].character
   else
     return false
   end
 end
 
-function goto_link_target(target)
-  local file_location, position = target:match('(.-)#(.*)')
-  local line_no, col_no = position:match('(%d+),(%d+)')
-  file_location = vim.fn.fnameescape(vim.uri_to_fname(file_location))  -- convert URI to file path
-  line_no = tonumber(line_no)
-  col_no = tonumber(col_no)
-  vim.cmd.edit(file_location)
-  vim.api.nvim_win_set_cursor(0, {line_no, col_no - 1})
+function jump_to_link_target(target)
+  local file_uri, line_no, col_no = target:match('(.-)#(%d+),(%d+)')
+  vim.cmd.edit(vim.fn.fnameescape(vim.uri_to_fname(file_uri)))
+  vim.api.nvim_win_set_cursor(0, {tonumber(line_no), tonumber(col_no) - 1})
 end
 
 return goto_link

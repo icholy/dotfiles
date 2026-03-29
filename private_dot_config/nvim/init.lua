333e9c90aa2cadd78bb0cdc8fba46a19203aaeba
Ilia Choly <ilia.choly@gmail.com>
2026-03-29 15:09:17 -0400

    nvim: replace vim.loop with vim.uv

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -76,7 +76,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- setup lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
